204911d3250a92e4ae2921721bfe717314b0c074
Alexandr Khoshin <t9216335043@gmail.com>
2019-03-28 18:19:09 +0300

    Промежуточно. Исправлено отношение точки к фигуре

--- src/ru/avalon/java/dev/j10/labs/shapes/Ellipse.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Ellipse.java
@@ -11,7 +11,7 @@ package ru.avalon.java.dev.j10.labs.shapes;
  * @see <a href="https://ru.wikipedia.org/wiki/%D0%AD%D0%BB%D0%BB%D0%B8%D0%BF%D1%81">Эллипс</a>
  */
 
-public interface Ellipse extends Shape, Point{
+public interface Ellipse extends Shape{
 
     /*
      * TODO: Закончить определение интерфейса 'Ellipse'
@@ -28,15 +28,6 @@ public interface Ellipse extends Shape, Point{
      */
     float getLength();
 
-    @Override
-    public default float getX() {
-        return 0; 
-    }
-
-    @Override
-    public default float getY() {
-        return 0; 
-    }
-    
+     
     
 }
